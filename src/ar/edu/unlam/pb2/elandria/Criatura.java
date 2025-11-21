b390b4e491eb2fb29d4849ea92cd983e3640f4a1
Santiago Orellana <santiagoorellana1905@gmail.com>
2025-11-21 18:57:41 -0300

    Implemetacion de logica de interaccion.

--- src/ar/edu/unlam/pb2/elandria/Criatura.java
+++ src/ar/edu/unlam/pb2/elandria/Criatura.java
@@ -53,5 +53,54 @@ public abstract class Criatura {
     public abstract void entrenar(int incremento);
 
     public abstract void pacificar();
+    
+    public void interactuarCon(Criatura otraCriatura) {
+        
+        boolean soyAncestral = this instanceof CriaturaAncestral;
+        boolean otraEsAncestral = otraCriatura instanceof CriaturaAncestral;
+
+        if (soyAncestral || otraEsAncestral) {
+            aplicarDominioAncestral(this, otraCriatura);
+            return;
+        }
+
+       
+        if (this.getAfinidad() == otraCriatura.getAfinidad()) {
+           
+            this.sumarEnergia(10);
+            otraCriatura.sumarEnergia(10);
+        } else if (sonAfinidadesOpuestas(this.getAfinidad(), otraCriatura.getAfinidad())) {
+            
+            this.setEstado(EstadoEmocional.INESTABLE);
+            otraCriatura.setEstado(EstadoEmocional.INESTABLE);
+        }
+        
+    }
+
+ 
+    private void aplicarDominioAncestral(Criatura c1, Criatura c2) {
+        
+        if (c1 instanceof CriaturaAncestral) {
+            c1.sumarEnergia(20);
+            c2.sumarEnergia(-15); 
+        } 
+        
+        if (c2 instanceof CriaturaAncestral) {
+            c2.sumarEnergia(20);
+            c1.sumarEnergia(-15);
+        }
+    }
+
+    private boolean sonAfinidadesOpuestas(AfinidadElemental a1, AfinidadElemental a2) {
+        if ((a1 == AfinidadElemental.AGUA && a2 == AfinidadElemental.FUEGO) ||
+            (a1 == AfinidadElemental.FUEGO && a2 == AfinidadElemental.AGUA)) {
+            return true;
+        }
+        if ((a1 == AfinidadElemental.AIRE && a2 == AfinidadElemental.TIERRA) ||
+            (a1 == AfinidadElemental.TIERRA && a2 == AfinidadElemental.AIRE)) {
+            return true;
+        }
+        return false;
+    }
 }
 
